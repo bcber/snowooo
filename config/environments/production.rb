724d8bcfe869b4a114852802340bea431404ba92
nxbtch <nxbtch1995@gmail.com>
2014-11-26 16:23:51 +0800

    server static on

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
